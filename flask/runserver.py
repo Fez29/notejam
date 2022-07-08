5782d236ae8b2b07c7453deae4541450bf81b930
Gareth Fraser <fez29@protonmail.com>
2022-07-08 21:13:22 +0200

    added changes to enable use of MySql and environment variables

--- flask/runserver.py
+++ flask/runserver.py
@@ -1,5 +1,11 @@
 from notejam import app
+import os
 
+username_env_var = os.environ.get('username', 'defaultuser')
+password_env_var = os.environ.get('password', 'defaultpassword')
+server_env_var = os.environ.get('server', 'defaultserver')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{username_env_var}:{password_env_var}@{server_env_var}/testdb".format(username, password, server)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
